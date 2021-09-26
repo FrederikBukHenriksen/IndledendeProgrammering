a115789294966a65793ecac6b8056185e087101b
Frederik Buk Henriksen <frederikbukhenriksen@gmail.com>
2021-09-26 12:15:50 +0200

    fix for større numre end 'long'

--- NumberCheck.java
+++ NumberCheck.java
@@ -6,24 +6,27 @@ Der står i opgaven, at det er decimal digits, så det er vel også kommatal
 test af commits
 */
 
+import java.math.BigInteger;
 
 public class NumberCheck {
-    public static void main(String[] args) {
-        System.out.print(check("3475"));
-        // System.out.print(check(""));
-    }
+    // public static void main(String[] args) {
+    //     System.out.print(check("165546168632215835423155415435"));
+    //     // System.out.print(check("3475"));
+
+    //     // System.out.print(check("3.475E3"));
+    //     // System.out.print(check("-3475"));       //DEN KAN IKKE KLARE NEGATIVE TAL.
+
+    // }
 
     public static boolean check (String number) {
         int[] array_new = ReverseStringToIntArray(number);
         return checkProcedure(array_new);
     }
 
-
-
     public static boolean checkProcedure(int[] input){
         //Double every digit
         for (int i = 1; i < input.length; i=i+2){
-            input[i]=input[i]*2;
+            input[i]=(input[i]*2);
             
             //If 10 or greater, replace digit with its mod 10 plus 1
             if (input[i]>= 10){
@@ -31,8 +34,7 @@ public class NumberCheck {
             }
         }
 
-        //Convert array to int
-        int crossSum = 0;
+        long crossSum = 0;
         for (int i = 0; i < input.length; i++){
             crossSum += input[i];
         }
@@ -46,15 +48,16 @@ public class NumberCheck {
     }
 
     public static int[] ReverseStringToIntArray(String input){
-        //Convert string to string array
-        String[] stringArray = input.split("");
+        //Convert into string for easy split.
+        String[] stringArray = input.split(""); 
 
+        
         //Parse characters one-by-one to int in reverse order.
-        int[] intArray = new int [stringArray.length];
+        int[] stringArrayBuffer = new int [stringArray.length];
         for (int i = 0; i < stringArray.length; i++){
-            intArray[(stringArray.length-1)-i] = Integer.parseInt(stringArray[i]);
+            stringArrayBuffer[(stringArray.length-1)-i] = Integer.parseInt(stringArray[i]);
         }
-        return intArray;
+        return stringArrayBuffer;
     }
 
 }
