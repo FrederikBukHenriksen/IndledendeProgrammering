54552d3bc5c189aff1e39121c66e2555e11f1eab
Frederik Henriksen <FrederikBukHenriksen@gmail.com>
2021-09-30 20:58:36 +0200

    Udvikling af et map og bug fix

--- PredatorPrey.java
+++ PredatorPrey.java
@@ -1,35 +1,41 @@
 import java.awt.*; //Point
 import java.util.Random; //Random
 
-public class Predatorprey {
+
+public class PredatorPrey{
 
     public static void main(String[] args) {
-        runSimulation(20, 2, 100);
+        runSimulation(5, 2, 100);
     }
 
     public static void runSimulation(int n, int s, int t) {
-        checkParameters(n, -s, t);
+        checkParameters(n, s, t);
 
         // Init random positions
         Random random = new Random();
-        Point prey = new Point(random.nextInt(n), random.nextInt(n));
-        Point predator = new Point(random.nextInt(n), random.nextInt(n));
+        Point prey = new Point(random.nextInt(n)+1, random.nextInt(n)+1);
+        Point predator = new Point(random.nextInt(n)+1, random.nextInt(n)+1);
         displayPositions(prey, predator);
 
-        while (predator.distance(prey) != 0) {
+        int iterationCounter = 0;
+
+        do {
+            //Move prey
             prey.translate(random.nextInt(2 * s + 1) - s, random.nextInt(2 * s + 1) - s); // Normalt kører den fra 0-(s-1).
             outOfBoundsCorrection(prey, n); 
-            PredatorpreyTeleport(prey,  s,  n);
+            //PredatorpreyTeleport(prey,  s,  n);
 
-
-            // Hvad er forskellen i afstand fra pred. til prey?
-
+            //Move predator
             movePredator(prey, predator, s);
-            outOfBoundsCorrection(prey, n);
+            outOfBoundsCorrection(predator, n);
+
+            //Display and evaluate positions
             displayPositions(prey, predator);
+            displayMap(prey, predator, n);
             checkIfChaught(prey, predator);
 
-        }
+            iterationCounter++;
+        }while (predator.distance(prey) != 0 && iterationCounter <= t);
 
     }
 
@@ -39,7 +45,7 @@ public class Predatorprey {
         } else {
             System.out.print("Illegal Parameters!");
             System.exit(0);
-        }
+        }   
     }
 
     public static void displayPositions(Point prey, Point predator) {
@@ -49,19 +55,19 @@ public class Predatorprey {
         System.out.println();
     }
 
-    public static void outOfBoundsCorrection(Point point, int n) { // Kan jeg komme udenom at skulle indsætte n?
-        if (point.x < 0) {
-            point.setLocation(0, point.y);
+    public static void outOfBoundsCorrection(Point point, int n) {
+        if (point.x < 1) {
+            point.setLocation(1, point.y);
         }
-        if (point.x > n - 1) {
-            point.setLocation(n - 1, point.y);
+        if (point.x > n) {
+            point.setLocation(n, point.y);
         }
 
-        if (point.y < 0) {
-            point.setLocation(point.x, 0);
+        if (point.y < 1) {
+            point.setLocation(point.x, 1);
         }
-        if (point.y > n - 1) {
-            point.setLocation(point.x, n - 1);
+        if (point.y > n) {
+            point.setLocation(point.x, n);
         }
     }
 
@@ -88,13 +94,33 @@ public class Predatorprey {
     public static void PredatorpreyTeleport(Point prey, int s, int n){
         if (prey.x % s == 0 && prey.x % s == 0){
             Random random = new Random();
-            prey = new Point(random.nextInt(n), random.nextInt(n));
+            prey = new Point(random.nextInt(n)+1, random.nextInt(n)+1);
             System.out.println("TELEPORT");
 
         }
     }
 
-
+    public static void displayMap(Point prey, Point predator, int n){
+        for (int i = n; i >= 1; i--){
+            for (int j = 1; j <= n; j++){
+                if (predator.distance(j,i) == 0 && prey.distance(j,i) == 0){
+                    System.out.print("X");
+                } else {
+                    if (predator.distance(j,i) == 0){
+                        System.out.print("R");
+                    }
+                    if (prey.distance(j,i) == 0){
+                        System.out.print("B");
+                    }
+                    if (predator.distance(j,i) != 0 && prey.distance(j,i) != 0){
+                        System.out.print(".");
+                    }
+                }
+                System.out.print("\t");
+            }
+            System.out.println("");
+        }
+    }
 
 
 
