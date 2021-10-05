342dc9399db568ac59cacdbd9901ab049957baba
Frederik Buk Henriksen <frederikbukhenriksen@gmail.com>
2021-10-05 09:37:57 +0200

    bug fix med while

--- PredatorPrey.java
+++ PredatorPrey.java
@@ -1,69 +1,58 @@
-//Import libraries
 import java.awt.*; //Point
 import java.util.Random; //Random
-import java.util.Arrays; //Array
+import java.util.Arrays;
 
 public class PredatorPrey {
 
     public static void main(String[] args) {
-        runSimulation(20, 2, 100);
+        runSimulation(10, 2, 20);
     }
 
     public static void runSimulation(int n, int s, int t) {
-        // Check paramters validity
         checkParameters(n, s, t);
 
         // Init random positions
-        Random random = new Random(); // Declare random object from the library
-        Point prey = new Point(random.nextInt(n) + 1, random.nextInt(n) + 1); // Random range [0;n] by offset
-        Point predator = new Point(random.nextInt(n) + 1, random.nextInt(n) + 1); // Random range [0;n] by offset
-
-        // Display inital positions
+        Random random = new Random();
+        Point prey = new Point(random.nextInt(n) + 1, random.nextInt(n) + 1);
+        Point predator = new Point(random.nextInt(n) + 1, random.nextInt(n) + 1);
         displayPositions(prey, predator);
-        displayMap(prey, predator, n);
+        // displayMap(prey, predator, n);
 
-        // Iterational loop for moving prey and predator
-        while (!checkIfChaught(prey, predator) && t > 0) { //Active while pray has not been caught and the maximum number of iterations is not met.
+
+        while (!checkIfChaught(prey, predator) && t > 0){
             // Move prey
-            prey.translate(random.nextInt(2 * s + 1) - s, random.nextInt(2 * s + 1) - s); // Random range [-2;2] by offset
-            outOfBoundsCorrection(prey, n); // Check if prey is out of bounds after move
-            PredatorpreyTeleport(prey, random, s, n);
+            prey.translate(random.nextInt(2 * s + 1) - s, random.nextInt(2 * s + 1) - s); // Normalt kører den fra
+                                                                                          // 0-(s-1).
+            outOfBoundsCorrection(prey, n);
 
             // Move predator
-            movePredator(s, prey, predator);
-            outOfBoundsCorrection(predator, n); // Check if predator is out of bounds after move
+            if (prey.x - predator.x < 0) {
+                predator.translate(-s, 0);
+            } else if (prey.x - predator.x > 0) {
+                predator.translate(s, 0);
+            }
 
-            // Print and Display positions for the iteration
+            if (prey.y - predator.y < 0) {
+                predator.translate(0, -s);
+            } else if   (prey.y - predator.y > 0) {
+                predator.translate(0, s);
+            }
+            outOfBoundsCorrection(predator, n);
+
+            // Display and evaluate positions
             displayPositions(prey, predator);
-            displayMap(prey, predator, n); 
-
-            //Print catch-message if the prey has been caught
+            // displayMap(prey, predator, n);
             if (checkIfChaught(prey, predator))
                 System.out.println("Catch!");
 
             t--; // Decrement number of maximum iterations
         }
-        System.exit(0); // Terminates program when while-loop becomes inactive
-    }
-
-    private static void movePredator(int s, Point prey, Point predator) {
-        // Decide if predator should move in x-direction
-        if (prey.x - predator.x < 0) {
-            predator.translate(-s, 0);
-        } else if (prey.x - predator.x > 0) {
-            predator.translate(s, 0);
-        }
-        // Decide if predator should move in y-direction
-        if (prey.y - predator.y < 0) {
-            predator.translate(0, -s);
-        } else if (prey.y - predator.y > 0) {
-            predator.translate(0, s);
-        }
+        System.exit(0); // Terminates program
     }
 
     public static void checkParameters(int n, int s, int t) {
         if (n > 0 && s > 0 && t >= 0) {
-            System.out.println("n=" + n + " s'=" + s + " t=" + t);
+            System.out.println("n=" + n + " s=" + s + " t=" + t);
         } else {
             System.out.print("Illegal Parameters!");
             System.exit(0); // Terminates program
@@ -78,7 +67,6 @@ public class PredatorPrey {
     }
 
     public static void outOfBoundsCorrection(Point point, int n) {
-        // If the position exceeds the area, return the position it to the valid area
         if (point.x < 1)
             point.setLocation(1, point.y);
         if (point.x > n)
@@ -93,34 +81,28 @@ public class PredatorPrey {
         if (predator.distance(prey) == 0)
             return true;
         return false;
+
     }
 
-    public static void PredatorpreyTeleport(Point prey, Random random, int s, int n) {
-        //Check whether the prey's position is divisible.
-        if (prey.x % s == 0 && prey.x % s == 0) {
-            //Set a random position for the prey
-            prey.setLocation(random.nextInt(n) + 1, random.nextInt(n) + 1);
-            System.out.println("TELEPORT");
-        }
-    }
+    // public static void PredatorpreyTeleport(Point prey, int s, int n){
+    // if (prey.x % s == 0 && prey.x % s == 0){
+    // Random random = new Random();
+    // prey = new Point(random.nextInt(n)+1, random.nextInt(n)+1);
+    // System.out.println("TELEPORT");
+
+    // }
+    // }
 
     public static void displayMap(Point prey, Point predator, int n) {
-        // Declare a 2D array the size of the area
         String[][] map = new String[n][n];
-
-        //Insert '/t' in every element. Fill only works in 1D, hence why running though the array
-        for (int i = 0; i < map.length; i++)
-            Arrays.fill(map[i], "\t");
-
-        //Decide whether they are indivudally present or on the same position.    
-        if (checkIfChaught(prey, predator)) {
-            map[predator.x - 1][predator.y - 1] = "CATCH\t"; // Insert catch position into the array
+        for (String[] row: map)
+            Arrays.fill(row, "\t");
+        if (checkIfChaught(prey, predator)){
+            map[predator.x-1][predator.y-1] = "CATCH\t";
         } else {
-            map[prey.x - 1][prey.y - 1] = "PREY\t"; // Insert prey position into the array
-            map[predator.x - 1][predator.y - 1] = "PRED\t"; // Insert predator position into the array
+            map[prey.x-1][prey.y-1] = "PREY\t";
+            map[predator.x-1][predator.y-1] = "PRED\t";
         }
-        //Turn the 2D array into a single string. Afterwards return it properly line-by-line
-        System.out.println(Arrays.deepToString(map).replace("],", "]\n"));
+        System.out.println(Arrays.deepToString(map).replace("],","]\n"));
     }
-
 }
