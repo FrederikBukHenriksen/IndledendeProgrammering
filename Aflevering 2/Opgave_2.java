d79974fb41e0d8ab0b68114c015b1ef9ab23ec56
Frederik Buk Henriksen <frederikbukhenriksen@gmail.com>
2021-10-14 18:58:58 +0200

    opgave 2 done

---
+++ Aflevering 2/Opgave_2.java
@@ -0,0 +1,14 @@
+import TrianglePattern;
+
+public class Opgave_2 {
+    public static void main(String[] args) {
+        int[] init = { 17 };
+        TrianglePattern TP1 = new TrianglePattern(44, 20, init);
+        // System.out.println(TP1.getN());
+        // System.out.println(TP1.getValueAt(0, 0));
+        TP1.getValueAt(0, 0);
+
+        // System.out.println(Arrays.deepToString(map).replace("],", "]\n"));
+
+    }
+}
--- Aflevering 2/TrianglePattern.java
+++ Aflevering 2/TrianglePattern.java
@@ -1,40 +1,74 @@
-// Approaches:
-// Initialiser et tomt array 2D.
-//Lav enkelte 1D arrays, og append dem til sidst.
-//// setDefaultCloseOperationfs
+import java.util.Arrays; //Array
 
 public class TrianglePattern {
 
     private int n;
     private int h;
     private int[] initail;
+    private int[][] map;
 
     // Mappet skal retunerers som
     public TrianglePattern(int n, int h, int[] initial) {
         this.n = n;
         this.h = h;
         this.initail = initial;
-
+        this.map = new int[h][n];
         // Initaliser en 2d matrix på h*n.
-        int[][] map = new int[h][n];
-        // Løb hvert array og fyld det med 0
-        for (int i = 0; i < map.length; i++)
-            Arrays.fill(map[i], 0);
+        this.map = new int[h][n];
 
         // Smid inital-værderine ind på deres plads på øverste række.
         for (int i = 0; i < initial.length; i++) {
-            map[0][i] = initial[i];
+            map[0][initial[i]] = 1;
         }
-        // Lav den akutelle linje, ved at evaluere linjen over.
-        // gangen.
+
         for (int i = 1; i < h; i++) {// Evaluer hvér linje, pånær den som initialiseres.
             for (int j = 1; j < n - 1; j++) { // Evaluer hvér position hvor der er plads til cursoren.
-                // Man kunne skrive dét som kursoren aflæser, og sende det over til en
-                // switchcase.
-
+                map[i][j] = cursor(map[i - 1][j - 1], map[i - 1][j], map[i - 1][j + 1]);
             }
 
         }
+        // System.out.println(Arrays.deepToString(map).replace("],", "]\n"));
 
+    }
+
+    private int cursor(int r1, int r2, int r3) {
+        // Construer de tre aflæsninger
+        String read = String.valueOf(r1) + String.valueOf(r2) + String.valueOf(r3);
+        switch (read) {
+            case "000":
+                return 0;
+            case "001":
+                return 1;
+            case "010":
+                return 1;
+            case "011":
+                return 1;
+            case "100":
+                return 1;
+            case "101":
+                return 0;
+            case "110":
+                return 0;
+            case "111":
+                return 0;
+            default:
+                return 0;
+        }
+    }
+
+    public int getValueAt(int r, int c) {
+        return map[r][c];
+    }
+
+    public int getN() {
+        return this.n;
+    }
+
+    public int getH() {
+        return this.h;
+    }
+
+    public int[] getInitial() {
+        return this.initail;
     }
 }
